f087030bf4e94419864233283c33e5ef475713b3
estellad <estelladong729@gmail.com>
2024-04-25 17:57:41 +0200

    update typo in xenium make data files

--- inst/scripts/make-data-Janesick-breastCancer-Xenium-rep2.R
+++ inst/scripts/make-data-Janesick-breastCancer-Xenium-rep2.R
@@ -65,7 +65,7 @@ all(c("cell_feature_matrix.h5", "cell_feature_matrix", "cells.csv.gz") %in%
 
 xe2_spe <- SpatialExperimentIO::readXeniumSXE(here::here("raw_data/xenium_sample1rep2/outs"), return_type = "SPE")
 xe2_spe <- xe2_spe[rowData(xe2_spe)$Type == "Gene Expression"]
-rownames(xe2) <- rowData(xe2)$Symbol
+rownames(xe2_spe) <- rowData(xe2_spe)$Symbol
 
 # ------------------
 # Saving data object
